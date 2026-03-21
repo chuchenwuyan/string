c410361e626d0651a8f21f7d4a10f9fe4cf63acd
chuchen wuyan <chuchen wuyan@WINDO>
2026-03-21 14:47:06 +0800

    添加项目文件。

---
+++ 字符串/002.c
@@ -0,0 +1,14 @@
+//键盘录入字符串，在控制台遍历
+#include <stdio.h>
+
+int main()
+{
+	char str[100];
+	//char* str;	这样写就会报错
+	//char* str = NULL；	不报错
+	//但char* str是只读常量区，不能键盘录入
+
+	printf("请录入\n");
+	scanf("%s",str);	//可以不写&
+	return 0;
+}
